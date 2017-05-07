2564ad03217905892422368dac5b0382c664e8a1
Ryan <ryanjonesirl@gmail.com>
2017-05-07 15:27:28 -0700

    add the new-animal component, this will handle adding new animals to firebase

--- src/app/new-animal/new-animal.component.ts
+++ src/app/new-animal/new-animal.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Animal } from '../animal.model';
+import { AnimalService } from '../animal.service';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-new-animal',
   templateUrl: './new-animal.component.html',
-  styleUrls: ['./new-animal.component.css']
+  styleUrls: ['./new-animal.component.css'],
+  providers: [ AnimalService ]
 })
 export class NewAnimalComponent {
 
+  constructor(private animalService: AnimalService) { }
+
+  submitForm(newSpecies: string, newName: string, newAge: string, newDiet: string, newCaretaker: string, newSex: string, newLikes: string, newDislikes: string) {
+    var newAnimal: Animal = new Animal(newSpecies, newName, newAge, newDiet, newCaretaker, newSex, newLikes, newDislikes);
+    this.animalService.addAnimal(newAnimal);
+  }
 
 }
