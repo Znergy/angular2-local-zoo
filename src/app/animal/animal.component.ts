07ac4aaeb25bd06413934c49585c62091721b45a
Ryan <ryanjonesirl@gmail.com>
2017-05-07 15:24:49 -0700

    add the animal component which will show details about a specific animal

--- src/app/animal/animal.component.ts
+++ src/app/animal/animal.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Animal } from '../animal.model';
+import { AnimalService } from '../animal.service';
 
 @Component({
   selector: 'app-animal',
   templateUrl: './animal.component.html',
-  styleUrls: ['./animal.component.css']
+  styleUrls: ['./animal.component.css'],
+  providers: [ AnimalService ]
 })
 export class AnimalComponent {
+  animalId: string;
+  animalToDisplay;
 
+  constructor(private route: ActivatedRoute, private location: Location, private animalService: AnimalService) { }
+
+  ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+     this.animalId = urlParameters['id'];
+   });
+   this.animalToDisplay = this.animalService.getAnimalById(this.animalId);
+  }
 }
