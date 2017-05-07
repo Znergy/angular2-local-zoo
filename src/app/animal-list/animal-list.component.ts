60ca142e6b1879f3a74199cfe34378735981d088
Ryan <ryanjonesirl@gmail.com>
2017-05-07 15:28:05 -0700

    add animal-list component which will show all animals

---
+++ src/app/animal-list/animal-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Animal } from '../animal.model';
+import { Router } from '@angular/router';
+import { AnimalService } from '../animal.service';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@Component({
+  selector: 'app-animal-list',
+  templateUrl: './animal-list.component.html',
+  styleUrls: ['./animal-list.component.css'],
+  providers: [ AnimalService ]
+})
+
+export class AnimalListComponent implements OnInit {
+  animals: FirebaseListObservable<any[]>;
+
+  constructor(private router: Router, private animalService: AnimalService){}
+
+  ngOnInit() {
+    this.animals = this.animalService.getAnimals();
+  }
+
+  goToDetailPage(clickedAnimal) {
+    this.router.navigate(['animals', clickedAnimal.$key]);
+  };
+}
